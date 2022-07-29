83a46c09a3f468d3ae7fd756c92374753619d8e0
Justin Worrall <justin.worrall@gmail.com>
2022-07-29 08:06:34 +0100

    abstracted grid

--- octavox/modules/project.py
+++ octavox/modules/project.py
@@ -54,6 +54,35 @@ class SVOffset:
         self.value+=value
         self.count+=1
 
+class ModGrid(dict):
+    
+    @classmethod
+    def randomise(self, modnames):
+        sz=int(math.ceil(len(modnames)**0.5))
+        coords=sorted([(x, y)
+                       for y in range(sz)
+                       for x in range(sz)],
+                      key=lambda x: random.random())[:len(modnames)]
+        return ModGrid({mod:xy
+                        for mod, xy in zip(modnames, coords)})
+    
+    def __init__(self, item={}):
+        dict.__init__(self, item)
+
+    def rms_distance(self, links):
+        total=0
+        for link in links:
+            a, b = [self[modname]
+                    for modname in link]
+            distance=((a[0]-b[0])**2+(a[1]-b[1])**2)**0.5
+            total+=distance
+        return total
+
+    def normalise(self):
+        return {k: tuple([v1-v0
+                          for v1, v0 in zip(v, self[Output])])
+                for k, v in self.items()}
+        
 class SVProject:
 
     def random_color(self, offset):
@@ -73,32 +102,8 @@ class SVProject:
                 for rgb in  color]
 
     def init_layout(self, modconfig, n=10000):
-        class Grid(dict):
-            @classmethod
-            def randomise(self, modnames):
-                sz=int(math.ceil(len(modnames)**0.5))
-                coords=sorted([(x, y)
-                               for y in range(sz)
-                               for x in range(sz)],
-                              key=lambda x: random.random())[:len(modnames)]
-                return Grid({mod:xy
-                             for mod, xy in zip(modnames, coords)})
-            def __init__(self, item={}):
-                dict.__init__(self, item)
-            def rms_distance(self, links):
-                total=0
-                for link in links:
-                    a, b = [self[modname]
-                            for modname in link]
-                    distance=((a[0]-b[0])**2+(a[1]-b[1])**2)**0.5
-                    total+=distance
-                return total
-            def normalise(self):
-                return {k: tuple([v1-v0
-                                  for v1, v0 in zip(v, self[Output])])
-                        for k, v in self.items()}
         def randomise(modnames, links):
-            grid=Grid.randomise(modnames)
+            grid=ModGrid.randomise(modnames)
             distance=grid.rms_distance(links)
             return (grid.normalise(), distance)
         modnames=[mod["name"] for mod in modconfig["modules"]]
