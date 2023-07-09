fd9584b9804619b94ccdbaba4ae576cc84f15f40
Justin Worrall <justin.worrall@gmail.com>
2023-07-09 20:16:09 +0100

    modified samples'

--- octavox/projects/picobeats/samples.py
+++ octavox/projects/picobeats/samples.py
@@ -1,53 +1,51 @@
-import os, random, yaml, zipfile
+import os, random, zipfile
 
 Instruments="kk|sn|ht".split("|")
 
+class Pool(dict):
+
+    def __init__(self, item={}):
+        dict.__init__(self, item)
+
+class Bank:
+
+    def __init__(self, name, zipfile):
+        self.name=name
+        self.zipfile=zipfile
+
+    @property
+    def wavfiles(self):
+        return [item.filename
+                for item in self.zipfile.infolist()]
+
+    def spawn_free(self, keys=Instruments):
+        wavfiles=self.wavfiles
+        return Pool({key:wavfiles
+                     for key in keys})
+
+    def spawn_curated(self, patterns=Patterns):
+        pool, wavfiles = Pool(), self.wavfiles
+        for key in Instruments:
+            pool.setdefault(key, [])
+            for wavfile in wavfiles:
+                if matches(wavfiles, patterns[key]):
+                    pool[key].append(wavfile)
+        return pool
+                
 class Banks(dict):
 
     def __init__(self, root):
         dict.__init__(self)        
         for bankfile in os.listdir(root):
+            name=bankfile.split(".")[0]
             path="%s/%s" % (root, bankfile)
-            self[bankfile]=zipfile.ZipFile(path)
+            bank=Bank(name, zipfile.ZipFile(path))
+            self[name]=bank
 
     def lookup(self, item):
         bankfile, wavfile = item
         return self[bankfile].open(wavfile, 'r')
-            
-class PoolBase:
 
-    def __init__(self):
-        pass
-
-    def randomise(self, banks, instrument):
-        return random.choice(self.mapping[instrument])
-    
-class SimplePool(PoolBase):
-
-    def __init__(self, banks, bankfile, instruments=Instruments):
-        PoolBase.__init__(self)
-        samplekeys=[[bankfile, item.filename]
-                    for item in banks[bankfile].infolist()]
-        self.mapping={instrument:samplekeys
-                      for instrument in instruments}
-
-class CuratedPool(PoolBase):
-
-    def __init__(self, bankfile):
-        PoolBase.__init__(self)
-        self.mapping=yaml.safe_load(open(bankfile).read())
-                
 if __name__=="__main__":
-    def test_simple_pool(banks, bankfile="default.zip"):
-        pool=SimplePool(banks, bankfile)
-        item=pool.randomise(banks, "kk")        
-        with open("tmp/%s" % item[1], 'wb') as f:            
-            f.write(banks.lookup(item).read())
-    def test_curated_pool(banks, mappingfile="octavox/projects/picobeats/pools/default.yaml"):
-        pool=CuratedPool(mappingfile)
-        item=pool.randomise(banks, "kk")        
-        with open("tmp/%s" % item[1], 'wb') as f:            
-            f.write(banks.lookup(item).read())                          
     banks=Banks("octavox/projects/picobeats/banks")
-    # test_simple_pool(banks)
-    test_curated_pool(banks)
+    print (banks["default"].spawn_free())
