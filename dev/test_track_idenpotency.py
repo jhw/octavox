c013de8703e12fbbd06072f38879cb67004a9efd
Justin Worrall <justin.worrall@gmail.com>
2023-07-20 20:11:22 +0100

    added code to test idempotency

--- dev/test_track_idenpotency.py
+++ dev/test_track_idenpotency.py
@@ -8,15 +8,23 @@ def test_patch(pool):
                          n=1).pop()
     r0=json.dumps(p0, sort_keys=True)
     p1=p0.clone()
-    p1.mutate(limits={"pat": 1,
+    p1.mutate(limits={"pat": 0,
                       "slices": 0,
                       "style": 0,
-                      "seed": 0})
+                      "seed": 1})
     r1=json.dumps(p1, sort_keys=True)
     print (r0==r1)
-    
+
+def test_slice(key, pool):
+    s0=Slice.randomise(key, pool)
+    r0=json.dumps(s0, sort_keys=True)
+    s1=s0.clone()
+    s1.randomise_seed(limit=1)
+    r1=json.dumps(s1, sort_keys=True)
+    print (r0==r1)
 
 if __name__=="__main__":
     banks=Banks("octavox/banks/pico")
     pools=banks.spawn_pools().cull()
     test_patch(pools["global-curated"])
+    test_slice("kk", pools["global-curated"])
