46cc0b99f426770f18734040597f14dd9f784dec
Justin Worrall <justin.worrall@gmail.com>
2023-08-21 11:59:19 +0100

    now pushing controller values

--- dev/inspect_acid_demo.py
+++ dev/inspect_acid_demo.py
@@ -5,4 +5,7 @@ if __name__=="__main__":
     modules={module.name:module for module in project.modules}
     mm=modules["acid bass"]
     for mod in mm.project.modules:
-        print (mod)
+        if mod:
+            print ("--- %s ---" % mod)
+            print (mod.controller_values)
+               
