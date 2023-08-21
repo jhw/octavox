d1a249d98f7de68c2386c25efb9225473b2368b7
Justin Worrall <justin.worrall@gmail.com>
2023-08-21 08:56:33 +0100

    fixed fadeout

--- dev/hello_pydub.py
+++ dev/hello_pydub.py
@@ -3,11 +3,11 @@ from pydub import AudioSegment
 import io, zipfile
 
 if __name__=="__main__":
-    zf=zipfile.ZipFile("octavox/banks/pico/default.zip")
+    zf=zipfile.ZipFile("tmp/banks/default.zip")
     wavfile=zf.open("63 FUGUE.wav", 'r')
     audio=AudioSegment.from_file(io.BytesIO(wavfile.read()))
     print (audio.duration_seconds)
     buf=io.BytesIO()
-    audio[:1000].export(buf, format="wav")
+    audio[:1000].fade_out(20).export(buf, format="wav")
     with open("tmp/fugue.wav", 'wb') as f:
         f.write(buf.getvalue())
