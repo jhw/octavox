a1a0a86e2b6ff9c6b109a3952beae79989b12799
Justin Worrall <justin.worrall@gmail.com>
2023-11-01 07:10:28 +0000

    removed samples subdir from sequencers

--- octavox/machines/sequencers/euclid.py
+++ octavox/machines/sequencers/euclid.py
@@ -4,7 +4,7 @@ from octavox.core.pools import SVSample
 
 from octavox.machines import Q
 
-from octavox.machines.sequencers.samples import SampleSequencer, mean_revert
+from octavox.machines.sequencers import SampleSequencer, mean_revert
 
 import yaml
 
@@ -78,7 +78,7 @@ def bjorklund(steps, pulses, **kwargs):
     pattern = pattern[i:] + pattern[0:i]
     return pattern
             
-class EuclidSampleSequencer(SampleSequencer):
+class EuclidSequencer(SampleSequencer):
     
     @classmethod
     def randomise(self,
@@ -89,17 +89,17 @@ class EuclidSampleSequencer(SampleSequencer):
                                                n=machine["params"]["nsamples"])
         seeds={k:SampleSequencer.random_seed()
                for k in "sample|trig|pattern|volume".split("|")}
-        return EuclidSampleSequencer({"name": machine["name"],
-                                      "class": machine["class"],
-                                      "params": machine["params"],
-                                      "samples": samples,
-                                      "seeds": seeds})
+        return EuclidSequencer({"name": machine["name"],
+                                "class": machine["class"],
+                                "params": machine["params"],
+                                "samples": samples,
+                                "seeds": seeds})
 
     def __init__(self, machine):
         SampleSequencer.__init__(self, machine)
                             
     def clone(self):
-        return EuclidSampleSequencer(self)
+        return EuclidSequencer(self)
 
     @mean_revert(attr="pattern")
     def random_pattern(self, q,
--- octavox/machines/sequencers/vitling.py
+++ octavox/machines/sequencers/vitling.py
@@ -8,7 +8,7 @@ from octavox.machines.sequencers.samples import SampleSequencer, mean_revert
 
 import yaml
 
-class VitlingSampleSequencer(SampleSequencer):
+class VitlingSequencer(SampleSequencer):
     
     @classmethod
     def randomise(self,
@@ -19,17 +19,17 @@ class VitlingSampleSequencer(SampleSequencer):
                                                n=machine["params"]["nsamples"])
         seeds={k:SampleSequencer.random_seed()
                for k in "sample|trig|pattern|volume".split("|")}
-        return VitlingSampleSequencer({"name": machine["name"],
-                                       "class": machine["class"],
-                                       "params": machine["params"],
-                                       "samples": samples,
-                                       "seeds": seeds})
+        return VitlingSequencer({"name": machine["name"],
+                                 "class": machine["class"],
+                                 "params": machine["params"],
+                                 "samples": samples,
+                                 "seeds": seeds})
 
     def __init__(self, machine):
         SampleSequencer.__init__(self, machine)
                             
     def clone(self):
-        return VitlingSampleSequencer(self)
+        return VitlingSequencer(self)
 
     @mean_revert(attr="pattern")
     def random_pattern(self, q):
